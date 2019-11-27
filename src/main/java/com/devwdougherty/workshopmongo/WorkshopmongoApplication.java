64badea9ee57071e32305ef9297c55fea9439216
devwdougherty <devwdougherty@outlook.com>
2019-11-26 21:42:18 -0300

    main class comment

--- src/main/java/com/devwdougherty/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/devwdougherty/workshopmongo/WorkshopmongoApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WorkshopmongoApplication {
 
+	/**
+	 * Main class.
+	 *
+	 * @param args
+	 * @author Willian Dougherty do Nascimento Barbosa
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopmongoApplication.class, args);
 	}
